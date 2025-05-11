f3bf6cb46e479509da28c56a5ef55a117ef3245e
lhm51 <396392408@qq.com>
2025-05-11 19:43:32 +0800

    谷爱凌技能测试

---
+++ extension/teest/extension.js
@@ -0,0 +1,40 @@
+import { lib, game, ui, get, ai, _status } from "../../noname.js";
+export const type = "extension";
+export default function(){
+	return {name:"teest",arenaReady:function(){
+    
+},content:function(config,pack){
+    
+},prepare:function(){
+    
+},precontent:function(){
+    
+},config:{},help:{},package:{
+    character: {
+        character: {
+            "谷爱凌": ["female","wei",4,["luoying","qixi"],[]],
+        },
+        translate: {
+            "谷爱凌": "谷爱凌",
+        },
+    },
+    card: {
+        card: {
+        },
+        translate: {
+        },
+        list: [],
+    },
+    skill: {
+        skill: {
+        },
+        translate: {
+        },
+    },
+    intro: "",
+    author: "无名玩家",
+    diskURL: "",
+    forumURL: "",
+    version: "1.0",
+},files:{"character":["谷爱凌.jpg"],"card":[],"skill":[],"audio":[]},connect:false} 
+};
